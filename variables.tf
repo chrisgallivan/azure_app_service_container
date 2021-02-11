2870779b68c9f7078db4f1958cad7297f7216373
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-02-11 17:14:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
 
+variable "resource_group_name" {
+  description = "Name of the resource group. Must be unique."
+  type        = string
+}
+variable "location_name" {
+  description = "Name of the Azure location."
+  type        = string
+}
+variable "app_service_plan_name" {
+  description = "Name of the app service plan. Must be unique."
+  type        = string
+}
+variable "app_service_name" {
+  description = "Name of the app service. Must be unique."
+  type        = string
+}
+variable "image_name" {
+  description = "Name of the docker image. Must be unique."
+  type        = string
+}
