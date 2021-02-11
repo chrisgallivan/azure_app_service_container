29b2636a5ce1b000553a86c7babb0faf9f435ecb
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-02-11 17:09:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
 
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|var.image_name"
+    always_on        = "true"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = false
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "DOCKER_ENABLE_CI"                    = true
+   
+  }
+}
